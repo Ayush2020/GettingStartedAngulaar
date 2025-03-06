8ba00d831e4161f00e40ed0e22d63d8b1d09afa6
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-03-06 22:21:05 +0530

    [Ayush] add . Launch BridgeLabzUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'HelloWorld';
-  imgUrl="C:/Users/ayush/Downloads/Ayush - FS HTML_CSS LP01 assets/assets";
+  title:string = 'HelloWorld'; 
+  imgUrl: string="https://imgs.search.brave.com/vbM4jQzRWT57hMn-O-1K9-TIHvgrR9BSmTvew_VRA8w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZTQub3dsZXIuY29t/L2xvZ28vYnJpZGdl/bGFiel9vd2xlcl8y/MDIxMDIyMl8xMDQx/NDVfbGFyZ2UucG5n";
+  url : string = 'https://www.bridgelabz.com';
+
 
   ngOnInit(): void {
     this.title = 'Hello From BridgeLabz!!';
+  }
+  
+  onClick(event: Event) : void {
+    window.open(this.url, '_blank');
   }
 }
