0bed15df77484d3ec4b356f6f01af4f3a1887506
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-03-06 22:32:43 +0530

    [Ayush] add . TwoWayBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   title:string = 'HelloWorld'; 
   imgUrl: string="https://imgs.search.brave.com/vbM4jQzRWT57hMn-O-1K9-TIHvgrR9BSmTvew_VRA8w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZTQub3dsZXIuY29t/L2xvZ28vYnJpZGdl/bGFiel9vd2xlcl8y/MDIxMDIyMl8xMDQx/NDVfbGFyZ2UucG5n";
   url : string = 'https://www.bridgelabz.com';
+  userName: string = '';
 
 
   ngOnInit(): void {
